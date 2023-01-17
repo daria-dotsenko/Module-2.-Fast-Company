9da0beaa19f152b790f574bda9f5ea3eddceea6c
dotsenkodaria <dotsenko8268@gmail.com>
2023-01-17 07:37:51 +0200

    Filtration

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -1,19 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { paginate } from "../utils/paginate";
 import Pagination from "./pagination";
 import User from "./user";
+import api from "../api";
 import PropTypes from "prop-types";
+import GroupList from "./groupList";
 
 const Users = ({ users, ...rest }) => {
+    const [currentPage, setCurrentPage] = useState(1);
+    const [professions, setProfessions] = useState(api.professions.fetchAll());
     const count = users.length;
     const pageSize = 4;
-    const [currentPage, setCurrentPage] = useState(1);
+    useEffect(() => {
+        console.log("render");
+    });
+    const handleProfessionSelect = (params) => {
+        console.log(params);
+    };
+    console.log(professions);
     const handlePageChange = (pageIndex) => {
         setCurrentPage(pageIndex);
     };
     const userCrop = paginate(users, currentPage, pageSize);
     return (
         <>
+            <GroupList
+                items={professions}
+                onItemSelect={handleProfessionSelect}
+            />
             {count > 0 && (
                 <table className="table">
                     <thead>
