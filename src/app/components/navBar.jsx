dbcb9ab6cf60516eeedcfdbdba94f20d220876f8
Daria Dotsenko <dotsenko8268@gmail.com>
2023-01-31 15:12:36 +0200

    Add Links

--- src/app/components/navBar.jsx
+++ src/app/components/navBar.jsx
@@ -1,32 +1,23 @@
 import React from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
         <ul className="nav">
             <li className="nav-item">
-                <a href="/" className="nav-link">
-                    Main
-                </a>
-                {/* <div className="nav-link"> */}
-                {/* <Link to="/">Main</Link> */}
-                {/* </div> */}
+                <div className="nav-link">
+                    <Link to="/">Main</Link>
+                </div>
             </li>
             <li className="nav-item">
-                <a href="/login" className="nav-link">
-                    Login
-                </a>
-                {/* <div className="nav-link"> */}
-                {/* <Link to="/login">Login</Link> */}
-                {/* </div> */}
+                <div className="nav-link">
+                    <Link to="/login">Login</Link>
+                </div>
             </li>
             <li className="nav-item">
-                <a href="/users" className="nav-link">
-                    Users
-                </a>
-                {/* <div className="nav-link"> */}
-                {/* <Link to="/users">Users</Link> */}
-                {/* </div> */}
+                <div className="nav-link">
+                    <Link to="/users">Users</Link>
+                </div>
             </li>
         </ul>
     );
