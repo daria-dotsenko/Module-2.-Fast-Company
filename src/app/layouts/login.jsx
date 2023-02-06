7efc05fe2da6d2be03af517abd9c303e3d55ea30
Daria Dotsenko <dotsenko8268@gmail.com>
2023-02-06 21:21:07 +0200

    2. Контролируемые и неконтролируемые компоненты

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const handleChange = (e) => {
+        setEmail(e.target.value);
+        console.log(e.target.value);
+    };
     return (
         <form action="">
             <div>
                 <label htmlFor="">Email</label>
-                <input type="text" id="email" />
+                <input
+                    type="text"
+                    id="email"
+                    value={email}
+                    onChange={handleChange}
+                />
             </div>
             <div>
-                <label htmlFor="">Password</label>
+                <label htmlFor="password">Password</label>
                 <input type="password" id="password" />
             </div>
         </form>
