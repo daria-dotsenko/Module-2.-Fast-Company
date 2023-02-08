4ce4de9baa759fdea3dd57555f3e3c385bc80136
Daria Dotsenko <dotsenko8268@gmail.com>
2023-02-08 09:08:42 +0200

    7. Отображение ошибки валидации

--- src/app/layouts/login.jsx
+++ src/app/layouts/login.jsx
@@ -3,7 +3,7 @@ import TextField from "../components/textField";
 
 const Login = () => {
     const [data, setData] = useState({ email: "", password: "" });
-    const [, setErrors] = useState();
+    const [errors, setErrors] = useState({});
     const handleChange = ({ target }) => {
         setData((prevState) => ({
             ...prevState,
@@ -41,6 +41,7 @@ const Login = () => {
                 name="email"
                 value={data.email}
                 onChange={handleChange}
+                error={errors.email}
             />
             <TextField
                 label="Password"
@@ -48,6 +49,7 @@ const Login = () => {
                 name="password"
                 value={data.password}
                 onChange={handleChange}
+                error={errors.password}
             />
             <button type="submit">Submit</button>
         </form>
