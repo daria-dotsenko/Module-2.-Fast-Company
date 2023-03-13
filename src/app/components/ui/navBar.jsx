5c1ca97e4f9d520006ce6f191281050d2f0f8bc8
Daria Dotsenko <dotsenko8268@gmail.com>
2023-03-13 10:53:29 +0200

    0.0.1 deploy on GitHub

--- src/app/components/ui/navBar.jsx
+++ src/app/components/ui/navBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import packageJson from "../../../../package.json";
 
 const NavBar = () => {
     return (
@@ -19,6 +20,7 @@ const NavBar = () => {
                     <Link to="/users">Users</Link>
                 </div>
             </li>
+            <li>{packageJson.version}</li>
         </ul>
     );
 };
