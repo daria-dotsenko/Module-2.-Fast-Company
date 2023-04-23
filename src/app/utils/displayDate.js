a4d61d487d58476fa9f517b225272ac6f24e148a
Daria Dotsenko <dotsenko8268@gmail.com>
2023-04-23 12:46:39 +0300

    2.14.25. Добавление HTTP-Сервиса (2)

--- src/app/utils/displayDate.js
+++ src/app/utils/displayDate.js
@@ -1,27 +1,37 @@
 export function displayDate(data) {
-    const date = new Date(parseInt(data));
-    const dateNow = new Date();
-    const yearDif = dateNow.getFullYear() - date.getFullYear();
-    if (yearDif === 0) {
-        const dayDif = dateNow.getDate() - date.getDate();
-        if (dayDif === 0) {
-            const hourDif = dateNow.getHours() - date.getHours();
-            if (hourDif === 0) {
-                const minutesDif = dateNow.getMinutes() - date.getMinutes();
-
-                if (minutesDif >= 0 && minutesDif < 5) return "1 минуту назад";
-                if (minutesDif >= 5 && minutesDif < 10) return "5 минут назад";
-                if (minutesDif >= 10 && minutesDif < 30) {
-                    return "10 минут назад";
-                }
-                return "30 минут назад";
-            }
-            return `${date.getHours()}:${date.getMinutes()}`;
-        }
-
-        return date.toLocaleString("default", { month: "long", day: "numeric" });
+    const currentDate = new Date();
+    const createdDate = new Date(parseInt(data));
+    const timeDiff = currentDate.getTime() - createdDate.getTime();
+    const secondsDiff = Math.round(timeDiff / 1000);
+    const monthNames = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    if (secondsDiff < 60) {
+        return "1 минуту назад";
+    } else if (secondsDiff < 300) {
+        return "5 минут назад";
+    } else if (secondsDiff < 1800) {
+        return "30 минут назад";
+    } else if (secondsDiff < 3600) {
+        return "менее 1 часа назад";
+    } else if (secondsDiff < 86400) { // hours
+        const hoursDiff = Math.floor(secondsDiff / 3600);
+        const minutesDiff = Math.floor((secondsDiff - hoursDiff * 3600) / 60);
+        const hours = [1, 21].includes(hoursDiff) ? "час"
+            : [2, 3, 4, 22, 23].includes(hoursDiff) ? "часа"
+                : "часов";
+        const minutes = [1, 21, 31, 41, 51].includes(minutesDiff) ? "минута"
+            : [2, 3, 4, 22, 23, 24, 32, 33, 34, 42, 43, 44, 52, 53, 54].includes(minutesDiff) ? "минуты"
+                : "минут";
+        return `${hoursDiff} ${hours} ${minutesDiff} ${minutes} назад`;
+    } else if (secondsDiff < 2592000) {
+        const currentMonthName = monthNames[createdDate.getMonth()];
+        const formattedDate = `${createdDate.getDate()} ${currentMonthName}`;
+        return formattedDate;
+    } else {
+        const currentMonthName = monthNames[createdDate.getMonth()];
+        const formattedDate = `${createdDate.getDate()} ${currentMonthName} ${createdDate.getFullYear()}`;
+        return formattedDate;
     }
-    return (
-        date.getFullYear() + "." + (date.getMonth() + 1) + "_" + date.getDate()
-    );
 }
