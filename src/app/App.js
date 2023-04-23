1402f544f38a29c5cdc9333c3760fe5db7625a76
Daria Dotsenko <dotsenko8268@gmail.com>
2023-04-23 13:40:33 +0300

    2.14.26. User Service

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Route, Switch, Redirect } from "react-router-dom";
-
 import Users from "./layouts/users";
 import Login from "./layouts/login";
 import Main from "./layouts/main";
 import NavBar from "./components/ui/navBar";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/" exact component={Main} />
                 <Redirect to="/" />
             </Switch>
+            <ToastContainer />
         </div>
     );
 }
--- src/app/hooks/useUsers.jsx
+++ src/app/hooks/useUsers.jsx
@@ -1,5 +1,8 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import userService from "../../service/user.service";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const UserContext = React.createContext();
 
@@ -8,9 +11,36 @@ export const useUser = () => {
 };
 
 const UserProvider = ({ children }) => {
+    const [users, setUsers] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    useEffect(() => {
+        getUsers();
+    }, []);
+    useEffect(() => {
+        if (error !== null) {
+            toast(error);
+            setError(null);
+        }
+    }, [error]);
+    async function getUsers() {
+        try {
+            const { content } = await userService.get();
+            console.log(content);
+            setUsers(content);
+            setLoading(false);
+        } catch (error) {
+            errorCatcher(error);
+        }
+    }
+    function errorCatcher(error) {
+        const { message } = error.response.data;
+        setError(message);
+        setLoading(false);
+    }
     return (
-        <UserContext.Provider>
-            {children}
+        <UserContext.Provider value={{ users }}>
+            {!isLoading ? children : "Loading..."}
         </UserContext.Provider>
     );
 };
