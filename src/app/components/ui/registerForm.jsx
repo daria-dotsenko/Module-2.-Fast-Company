ab11a0b625ce839be42591ee1d3d0a38654bdd62
Daria Dotsenko <dotsenko8268@gmail.com>
2023-04-26 12:00:02 +0300

    2.16.4. Рефакторинг формы регистрации

--- src/app/components/ui/registerForm.jsx
+++ src/app/components/ui/registerForm.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { validator } from "../../utils/validator";
 import TextField from "../common/form/textField";
-import api from "../../api";
 import SelectField from "../common/form/selectField";
 import RadioField from "../common/form/radioField";
 import MultiSelectField from "../common/form/multiSelectField";
 import CheckBoxField from "../common/form/checkBoxField";
+import { useQualities } from "../../hooks/useQualities";
+import { useProfessions } from "../../hooks/useProfession";
 
 const RegisterForm = () => {
     const [data, setData] = useState({
@@ -16,50 +17,40 @@ const RegisterForm = () => {
         qualities: [],
         licence: false
     });
-    const [qualities, setQualities] = useState([]);
-    const [professions, setProfession] = useState([]);
+    const { qualities } = useQualities();
+    const qualitiesList = qualities.map(q => ({
+        label: q.name,
+        value: q._id
+    }));
+    const { professions } = useProfessions();
+    const professionsList = professions.map(p => ({
+        label: p.name,
+        value: p._id
+    }));
     const [errors, setErrors] = useState({});
 
-    const getProfessionById = (id) => {
-        for (const prof of professions) {
-            if (prof.value === id) {
-                return { _id: prof.value, name: prof.label };
-            }
-        }
-    };
-    const getQualities = (elements) => {
-        const qualitiesArray = [];
-        for (const elem of elements) {
-            for (const quality in qualities) {
-                if (elem.value === qualities[quality].value) {
-                    qualitiesArray.push({
-                        _id: qualities[quality].value,
-                        name: qualities[quality].label,
-                        color: qualities[quality].color
-                    });
-                }
-            }
-        }
-        return qualitiesArray;
-    };
-
-    useEffect(() => {
-        api.professions.fetchAll().then((data) => {
-            const professionsList = Object.keys(data).map((professionName) => ({
-                label: data[professionName].name,
-                value: data[professionName]._id
-            }));
-            setProfession(professionsList);
-        });
-        api.qualities.fetchAll().then((data) => {
-            const qualitiesList = Object.keys(data).map((optionName) => ({
-                value: data[optionName]._id,
-                label: data[optionName].name,
-                color: data[optionName].color
-            }));
-            setQualities(qualitiesList);
-        });
-    }, []);
+    // const getProfessionById = (id) => {
+    //     for (const prof of professions) {
+    //         if (prof.value === id) {
+    //             return { _id: prof.value, name: prof.label };
+    //         }
+    //     }
+    // };
+    // const getQualities = (elements) => {
+    //     const qualitiesArray = [];
+    //     for (const elem of elements) {
+    //         for (const quality in qualities) {
+    //             if (elem.value === qualities[quality].value) {
+    //                 qualitiesArray.push({
+    //                     _id: qualities[quality].value,
+    //                     name: qualities[quality].label,
+    //                     color: qualities[quality].color
+    //                 });
+    //             }
+    //         }
+    //     }
+    //     return qualitiesArray;
+    // };
     const handleChange = (target) => {
         setData((prevState) => ({
             ...prevState,
@@ -116,13 +107,11 @@ const RegisterForm = () => {
         e.preventDefault();
         const isValid = validate();
         if (!isValid) return;
-        const { profession, qualities } = data;
-        console.log({
-            ...data,
-            profession: getProfessionById(profession),
-            qualities: getQualities(qualities)
-        });
+        console.log(data);
+        const newData = { ...data, qualities: data.qualities.map(q => q.value) };
+        console.log(newData);
     };
+
     return (
         <form onSubmit={handleSubmit}>
             <TextField
@@ -143,7 +132,7 @@ const RegisterForm = () => {
             <SelectField
                 label="Выбери свою профессию"
                 defaultOption="Choose..."
-                options={professions}
+                options={professionsList}
                 name="profession"
                 onChange={handleChange}
                 value={data.profession}
@@ -161,7 +150,7 @@ const RegisterForm = () => {
                 label="Выберите ваш пол"
             />
             <MultiSelectField
-                options={qualities}
+                options={qualitiesList}
                 onChange={handleChange}
                 defaultValue={data.qualities}
                 name="qualities"
---
+++ src/app/utils/mockData.js
@@ -0,0 +1,60 @@
+import { useEffect, useState } from "react";
+import professions from "../mockData/professions.json";
+import qualities from "../mockData/qualities.json";
+import users from "../mockData/users.json";
+import httpService from "../../service/http.service";
+
+const useMockData = () => {
+    const statusConst = {
+        idle: "Not started",
+        pending: "In process",
+        success: "Ready",
+        error: "Error occured"
+    };
+    const [error, setError] = useState(null);
+    const [status, setStatus] = useState(statusConst.idle);
+    const [progress, setProgress] = useState(0);
+    const [count, setCount] = useState(0);
+    const summCount = professions.length + qualities.length + users.length;
+    const incrementCount = () => {
+      setCount((prevState) => prevState + 1);
+    };
+    const updateProgress = () => {
+        if (count !== 0 && status === statusConst.idle) {
+            setStatus(statusConst.pending);
+        }
+        const newProgress = Math.floor((count / summCount) * 100);
+        if (progress < newProgress) {
+            setProgress(() => newProgress);
+        }
+        if (newProgress === 100) {
+            setStatus(statusConst.success);
+        }
+    };
+    useEffect(() => {
+        updateProgress();
+    }, [count]);
+    async function initialize() {
+        try {
+            for (const prof of professions) {
+                await httpService.put(`profession/${prof._id}`, prof);
+                incrementCount();
+            }
+            for (const user of users) {
+                await httpService.put(`user/${user._id}`, user);
+                incrementCount();
+            }
+            for (const quality of qualities) {
+                await httpService.put(`quality/${quality._id}`, quality);
+                incrementCount();
+            }
+        } catch (error) {
+            setError(error);
+            setStatus(statusConst.error);
+        }
+    }
+
+    return { error, initialize, progress, status };
+};
+
+export default useMockData;
