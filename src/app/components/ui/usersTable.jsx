3c2f9f8844b7e415cbb5a7516822f43abb6e2763
Daria Dotsenko <dotsenko8268@gmail.com>
2023-05-02 21:43:45 +0300

    2.17.16. Изменяем UserList Page

--- src/app/components/ui/usersTable.jsx
+++ src/app/components/ui/usersTable.jsx
@@ -11,7 +11,6 @@ const UserTable = ({
     onSort,
     selectedSort,
     onToggleBookMark,
-    onDelete,
     ...rest
 }) => {
     const columns = {
@@ -41,16 +40,6 @@ const UserTable = ({
                     onClick={() => onToggleBookMark(user._id)}
                 />
             )
-        },
-        delete: {
-            component: (user) => (
-                <button
-                    onClick={() => onDelete(user._id)}
-                    className="btn btn-danger"
-                >
-                    delete
-                </button>
-            )
         }
     };
     return (
@@ -67,8 +56,7 @@ UserTable.propTypes = {
     users: PropTypes.array.isRequired,
     onSort: PropTypes.func.isRequired,
     selectedSort: PropTypes.object.isRequired,
-    onToggleBookMark: PropTypes.func.isRequired,
-    onDelete: PropTypes.func.isRequired
+    onToggleBookMark: PropTypes.func.isRequired
 };
 
 export default UserTable;
