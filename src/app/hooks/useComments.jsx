796b2b343156663e016454c94d1ef94d3675b68d
Daria Dotsenko <dotsenko8268@gmail.com>
2023-05-03 21:58:10 +0300

    2.17.20. Добавляем метод создания комментария

--- src/app/hooks/useComments.jsx
+++ src/app/hooks/useComments.jsx
@@ -1,5 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import { useParams } from "react-router-dom";
+import { useAuth } from "./useAuth";
+import { nanoid } from "nanoid";
 // import { toast } from "react-toastify";
 
 const CommentsContext = React.createContext();
@@ -9,16 +12,27 @@ export const useComments = () => {
 };
 
 export const CommentsProvider = ({ children }) => {
+    const { userId } = useParams();
+    const { currentUser } = useAuth();
     // const [isLoading, setLoading] = useState(true);
     const [comments, setComments] = useState([]);
     // const [error, setError] = useState(null);
     useEffect(() => {
         setComments(null);
     }, []);
-
+    async function createComment(data) {
+        const comment = {
+            ...data,
+            _id: nanoid(),
+            pageId: userId,
+            crated_at: Date.now(),
+            userId: currentUser._id
+        };
+        console.log(comment);
+    }
     return (
         <CommentsContext.Provider
-            value={{ comments }}
+            value={{ comments, createComment }}
         >
             {children}
         </CommentsContext.Provider>
