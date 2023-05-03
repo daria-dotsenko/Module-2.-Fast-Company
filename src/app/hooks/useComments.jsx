5c245f0657e0dcc02271da1517567f10361e39e2
Daria Dotsenko <dotsenko8268@gmail.com>
2023-05-03 21:46:27 +0300

    2.17.19. Создаем Comment Hook

---
+++ src/app/hooks/useComments.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+// import { toast } from "react-toastify";
+
+const CommentsContext = React.createContext();
+
+export const useComments = () => {
+    return useContext(CommentsContext);
+};
+
+export const CommentsProvider = ({ children }) => {
+    // const [isLoading, setLoading] = useState(true);
+    const [comments, setComments] = useState([]);
+    // const [error, setError] = useState(null);
+    useEffect(() => {
+        setComments(null);
+    }, []);
+
+    return (
+        <CommentsContext.Provider
+            value={{ comments }}
+        >
+            {children}
+        </CommentsContext.Provider>
+    );
+};
+
+CommentsProvider.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.node),
+        PropTypes.node
+    ])
+};
