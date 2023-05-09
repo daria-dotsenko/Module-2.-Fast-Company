d871f4818c3500ec4934cd0d1291510840498523
Daria Dotsenko <dotsenko8268@gmail.com>
2023-05-09 22:40:59 +0300

    Redirects the user to change their page

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -1,19 +1,29 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { Redirect, useParams } from "react-router-dom";
 import EditUserPage from "../components/page/editUserPage";
 import UserPage from "../components/page/userPage";
 import UsersListPage from "../components/page/usersListPage";
 import UserProvider from "../hooks/useUsers";
+import { useAuth } from "../hooks/useAuth";
 
 const Users = () => {
     const params = useParams();
+    const { currentUser } = useAuth();
     const { userId, edit } = params;
     return (
         <>
             <UserProvider>
                 {userId ? (
                     edit ? (
-                        <EditUserPage />
+                        currentUser._id !== userId ? (
+                            <>
+                                <Redirect to={`/users/${currentUser._id}/edit`}/>
+                                <EditUserPage />
+                            </>
+                        )
+                        : (
+                                <EditUserPage />
+                            )
                     ) : (
                         <UserPage userId={userId} />
                     )
